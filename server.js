fc3e6798c405bb8c257ebce9807eb44e672f476f
Barsa999 <jhosflores2005@gmail.com>
2026-09-18 18:14:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/api/chat', async (req, res) => {
     try {
         const { mensaje } = req.body;
         const apiKey = process.env.GEMINI_API_KEY;
-        const googleUrl = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+        const googleUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
         const respuestaGoogle = await fetch(googleUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
